030b747d2a04033aa1d79f600b3ed00b800d829b
Telesheva <megoirlik11@gmail.com>
2020-03-17 13:27:12 +0200

    initial configurations

--- src/components/ExhibitionList/ExhibitionList.jsx
+++ src/components/ExhibitionList/ExhibitionList.jsx
@@ -1,24 +1,107 @@
 import React from "react";
 import "./ExhibitionList.scss";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
 
 const ExhibitionList = props => {
+  const useStyles = makeStyles({
+    root: {
+      maxWidth: 345
+    },
+    media: {
+      height: 140
+    }
+  });
+
+  const classes = useStyles();
+
   return (
     <>
+      <h1 className="exhibition-list__heading">Афиша выставок</h1>
       <ul className="exhibition-list">
         <li className="exhibition-list__item">
-          <img
-            src="https://muzei-mira.com/templates/museum/images/paint/devyatyy-val-ayvazovskiy+.jpg"
-            alt="pic-1"
-            className="exhibition-list__item_picture"
-          />
-          <div className="exhibition-list__item_info">
-            <p className="exhibition-list__item_info_text">
-              Выставка И.К. Айвазовского
-            </p>
-            <hr />
-            <p className="exhibition-list__item_info_text">C 18 по 25 марта</p>
-            <p className="exhibition-list__item_info_text">ХНАТОБ</p>
-          </div>
+          <Card className={classes.root}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image="https://muzei-mira.com/templates/museum/images/paint/devyatyy-val-ayvazovskiy+.jpg"
+                title="painting"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Выставка И.К. Айвазовского
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  Выставка пройдет с 18 по 25 марта в Харьковском Национальном
+                  Театре Оперы и Балета. Вход свободный, ждем Вас!
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </li>
+        <li className="exhibition-list__item">
+          <Card className={classes.root}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTl8cXIGAwmjVTZI9B88hhybAb4X1SppnloO1CPlxnfyIlk6HSq"
+                title="painting"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Выставка современных художников
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  Выставка пройдет с 18 по 25 марта в Харьковском Национальном
+                  Театре Оперы и Балета. Вход свободный, ждем Вас!
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </li>
+        <li className="exhibition-list__item">
+          <Card className={classes.root}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQzBIb-7iP_Xbf2OrNoXNvPZX6lcau7h4ALCwgdzyPa82dtXykU"
+                title="painting"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Выставка И.К. Айвазовского
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  Выставка пройдет с 18 по 25 марта в Харьковском Национальном
+                  Театре Оперы и Балета. Вход свободный, ждем Вас!
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </li>
+        <li className="exhibition-list__item">
+          <Card className={classes.root}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRGcS_4WFfi_-WrKMxstEDQhucPGFVku2whPzke5xVobt5eVeKU"
+                title="painting"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Выставка И.К. Айвазовского
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  Выставка пройдет с 18 по 25 марта в Харьковском Национальном
+                  Театре Оперы и Балета. Вход свободный, ждем Вас!
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
         </li>
       </ul>
     </>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./index.scss";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { compose, createStore, applyMiddleware } from "redux";
+import rootReducer from "./store/reducers/rootReducer";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+const app = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
+
+ReactDOM.render(app, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
